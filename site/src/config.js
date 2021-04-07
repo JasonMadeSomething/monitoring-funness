002a82e6f7a60d453979271dfb4d048e08c62109
Jason Teitelman <jasonmadesomething@gmail.com>
2021-04-06 20:26:42 -0400

    Change config to use react specific envirionment variable for whatever reason

--- site/src/config.js
+++ site/src/config.js
@@ -13,6 +13,5 @@ config.domains = {}
  * This will enable your front-end to communicate with your back-end.
  * (e.g. 'https://api.mydomain.com' or 'https://091jafsl10.execute-api.us-east-1.amazonaws.com')
  */
-config.domains.api = `https://${process.env.apidomain}`
-
+config.domains.api = `https://${process.env.REACT_APP_APIDOMAIN}`
 export default config
